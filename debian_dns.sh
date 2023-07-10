ecdab15df48390834b6967dde3bd9f4b7c925c43
Marcel Großmann <marcel.grossmann@uni-bamberg.de>
2023-07-10 14:38:16 +0200

    Updates

--- debian_dns.sh
+++ debian_dns.sh
@@ -1,21 +1,32 @@
 #!/usr/bin/env bash
 
 # Script needs to be executed as admin
+SUDO=''
+if (( $EUID != 0 )); then
+    SUDO='sudo'
+fi
 
 # Disable systemd-resolved
-systemctl disable systemd-resolved
-systemctl stop systemd-resolved
-unlink /etc/resolv.conf
+$SUDO systemctl disable systemd-resolved
+$SUDO systemctl stop systemd-resolved
+$SUDO unlink /etc/resolv.conf
 # Activate the dnsmasq plugin
-cat <<CONF | tee /etc/NetworkManager/conf.d/00-use-dnsmasq.conf
+cat <<CONF | $SUDO tee /etc/NetworkManager/conf.d/00-use-dnsmasq.conf
 [main]
 dns=dnsmasq
 CONF
 # Setup the public DNS and the `.test` pseudo-TLD
-cat <<CONF | tee /etc/NetworkManager/dnsmasq.d/00-dns-public.conf
+cat <<CONF | $SUDO tee /etc/NetworkManager/dnsmasq.d/00-dns-public.conf
 server=1.1.1.1
 CONF
-cat <<CONF | tee /etc/NetworkManager/dnsmasq.d/00-address-local.conf
+cat <<CONF | $SUDO tee /etc/NetworkManager/dnsmasq.d/00-address-local.conf
 address=/.test/127.0.0.1
 CONF
-systemctl restart NetworkManager
+cat <<CONF | $SUDO tee /etc/resolv.conf
+nameserver 127.0.0.1
+CONF
+cat <<CONF | $SUDO tee -a /etc/dnsmasq.conf
+conf-dir=/etc/NetworkManager/dnsmasq.d/,*.conf
+CONF
+$SUDO systemctl restart NetworkManager
+$SUDO systemctl restart dnsmasq
---
+++ kaas_aliases.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Source this file to uses aliases
+# source kaas_aliases.sh
+
+alias kaas="docker compose -f kaas.yml"
+alias kaas.dev="docker compose -f kaas.dev.yml"
