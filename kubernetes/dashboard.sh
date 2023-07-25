cad12294e09d018a8cdfa6886502664bfdc1634f
Marcel Großmann <whatever4711@gmail.com>
2023-07-25 18:06:55 +0200

    Updates on Automatic Setup

--- kubernetes/dashboard.sh
+++ kubernetes/dashboard.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 showtoken=1
 # not working with k3s:
-cmd="kubectl proxy"
+cmd="kubectl"
 count=`pgrep -cf "$cmd"`
 version_kube_dashboard="v2.7.0"
 dashboard_yaml="https://raw.githubusercontent.com/kubernetes/dashboard/${version_kube_dashboard}/aio/deploy/recommended.yaml"
-msgstarted="-e Kubernetes Dashboard \e[32mstarted\e[0m"
-msgstopped="-e Kubernetes Dashboard \e[31mstopped\e[0m"
-msginstalled="-e K3s successfully \e[32minstalled\e[0m"
+msgstarted="Kubernetes Dashboard \e[32mstarted\e[0m"
+msgstopped="Kubernetes Dashboard \e[31mstopped\e[0m"
+msginstalled="K3s successfully \e[32minstalled\e[0m"
 
 # TODO Skooner?
 
 function install_k3s(){
   curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644
-  echo $msginstalled
+  echo -e "$msginstalled"
 }
 
 function start_dashboard() {
@@ -23,8 +23,8 @@ function start_dashboard() {
     kubectl apply -f ./dashboard/dashboard.read-only.yml >/dev/null 2>&1
 
     if [ $count = 0 ]; then
-       nohup $cmd >/dev/null 2>&1 &
-       echo $msgstarted
+       nohup "$cmd" >/dev/null 2>&1 &
+       echo -e "$msgstarted"
     else
        echo "Kubernetes Dashboard already running"
     fi
@@ -32,14 +32,14 @@ function start_dashboard() {
 
 function stop_dashboard() {
     showtoken=0
-    if [ $count -gt 0 ]; then
+    if [ "$count" -gt 0 ]; then
        kill -9 $(pgrep -f "$cmd")
     fi
     kubectl delete -f $dashboard_yaml >/dev/null 2>&1
     kubectl delete -f ./dashboard/dashboard.admin-user.yml >/dev/null 2>&1
     kubectl delete -f ./dashboard/dashboard.admin-user-role.yml >/dev/null 2>&1
     kubectl delete -f ./dashboard/dashboard.read-only.yml >/dev/null 2>&1
-    echo $msgstopped
+    echo -e "$msgstopped"
 }
 
 case $1 in
@@ -53,18 +53,18 @@ stop)
   stop_dashboard
   ;;
 status)
-  found=`kubectl get serviceaccount admin-user -n kubernetes-dashboard 2>/dev/null`
+  found=$(kubectl get serviceaccount admin-user -n kubernetes-dashboard 2>/dev/null)
   if [[ $count = 0 ]] || [[ $found = "" ]]; then
      showtoken=0
-     echo $msgstopped
+     echo -e "$msgstopped"
   else
-     found=`kubectl get clusterrolebinding admin-user -n kubernetes-dashboard 2>/dev/null`
+     found=$(kubectl get clusterrolebinding admin-user -n kubernetes-dashboard 2>/dev/null)
      if [[ $found = "" ]]; then
         nopermission=" but user has no permissions."
-        echo $msgstarted$nopermission
+        echo -e "$msgstarted""$nopermission"
         echo 'Run "dashboard start" to fix it.'
      else
-        echo $msgstarted
+        echo -e "$msgstarted"
      fi
   fi
   ;;
@@ -78,7 +78,7 @@ if [ $showtoken -gt 0 ]; then
 
    # Show token
    echo "Admin token (permanent):"
-    kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard describe serviceaccounts admin-user | grep 'Tokens' | awk '{ print $2 }') | grep 'token:' | awk '{ print $2}'
+    kubectl -n kubernetes-dashboard describe secret "$(kubectl -n kubernetes-dashboard describe serviceaccounts admin-user | grep 'Tokens' | awk '{ print $2 }')" | grep 'token:' | awk '{ print $2}'
    echo
 
    echo "User read-only token (expires):"
--- kubernetes/megalos.sh
+++ kubernetes/megalos.sh
@@ -4,19 +4,20 @@ multus_yml="https://raw.githubusercontent.com/k8snetworkplumbingwg/multus-cni/v4
 flannel_yml="https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml"
 proxy_port=8001
 proxy_address="localhost"
+kathara_config="${HOME}/.config/kathara.conf"
 
 function start_megalos(){
-  kubectl apply -f ${flannel_yml} >/dev/null 2>&1
+  #kubectl apply -f ${flannel_yml} >/dev/null 2>&1
   kubectl apply -f ${multus_yml} >/dev/null 2>&1
   kubectl apply -f ${megalos_yml} >/dev/null 2>&1
-  kubectl apply -f ./megalos/network.yml >/dev/null 2>&1
+  #kubectl apply -f ./megalos/network.yml >/dev/null 2>&1
 }
 
 function stop_megalos(){
-  kubectl delete -f ./megalos/network.yml >/dev/null 2>&1
+  #kubectl delete -f ./megalos/network.yml >/dev/null 2>&1
   kubectl delete -f ${megalos_yml} >/dev/null 2>&1
   kubectl delete -f ${multus_yml} >/dev/null 2>&1
-  kubectl delete -f ${flannel_yml} >/dev/null 2>&1
+  #kubectl delete -f ${flannel_yml} >/dev/null 2>&1
 }
 
 function api_setup(){
@@ -24,20 +25,37 @@ function api_setup(){
   # Accessing the KUBE-API https://kubernetes.io/docs/tasks/administer-cluster/access-cluster-api/
   # TODO check if proxy is already running
   kubectl proxy --address=$proxy_address --port=$proxy_port  >/dev/null 2>&1 &
-  echo "Proxy: "
-  echo "http://${proxy_address}:${proxy_port}"
+  proxy="http://${proxy_address}:${proxy_port}"
+  printf "Proxy:\n%s\n" "${proxy}"
 
-  echo "API-Server: "
-  kubectl config view -o jsonpath="{.clusters[?(@.name==\"default\")].cluster.server}"
+
+  api_server=$(kubectl config view -o jsonpath="{.clusters[?(@.name==\"default\")].cluster.server}")
   # TODO: Access Token for Kathara
-  kubectl apply -f ./megalos/kathara-token.yml >/dev/null 2>&1
-  echo
+  printf "API-Server:\n%s\n" "${api_server}"
 
   # Get the token value
-  echo "Token for Kathara: "
-  kubectl get secret kathara-token -o jsonpath='{.data.token}' | base64 --decode
-  echo
+  kubectl apply -f ./megalos/kathara-token.yml >/dev/null 2>&1
+  token=$(kubectl get secret kathara-token -o jsonpath='{.data.token}' | base64 --decode)
+  printf "Token for Kathara:\n%s\n" "${token}"
 
+  while true; do
+      read -p "Do you wish to setup Kathara (y/n)? " yn
+      case $yn in
+          [Yy]* ) config_kathara "${proxy}" "${token}"; break;;
+          [Nn]* ) exit;;
+          * ) echo "Please answer yes or no.";;
+      esac
+  done
+
+}
+
+function config_kathara(){
+  jq '.manager_type = "kubernetes"' "${kathara_config}" > new_kathara.conf
+  jq --arg a "${1}" '.api_server_url=$a' new_kathara.conf > tmp.conf
+  jq --arg b "${2}" '.api_token=$b' tmp.conf > new_kathara.conf
+  rm tmp.conf
+  jq '.' new_kathara.conf
+  mv new_kathara.conf "${HOME}"/.config/kathara.conf
 }
 
 function attach_shell(){
