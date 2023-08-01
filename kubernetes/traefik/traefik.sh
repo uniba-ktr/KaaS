a7e8e23ae2a510c1808a39541b2a964b37acf1a7
Marcel Großmann <whatever4711@gmail.com>
2023-08-01 23:08:34 +0200

    updates on ingress

--- kubernetes/traefik/traefik.sh
+++ kubernetes/traefik/traefik.sh
@@ -5,7 +5,8 @@ function start_traefik() {
                 -f 00-account.yml \
                 -f 01-role-binding.yml \
                 -f 02-traefik.yml \
-                -f 02-traefik-services.yml
+                -f 02-traefik-services.yml \
+                -f 02-traefik-ingress.yml
   kubectl apply -f 03-whoami.yml \
                 -f 03-whoami-services.yml \
                 -f 04-whoami-ingress.yml
